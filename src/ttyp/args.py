2e42cf4bc86f37a5ff5a44aa8486c8376d98240b
Felipe Jara <fjararibet@gmail.com>
2025-06-10 22:12:16 -0400

    validate and list languages

--- src/ttyp/args.py
+++ src/ttyp/args.py
@@ -1,4 +1,12 @@
 import argparse
+from .content import get_available_languages
+
+
+def valid_language(language):
+    if language not in get_available_languages():
+        raise argparse.ArgumentTypeError(
+            f"invalid choice: {language!r} (use -L to see valid languages)")
+    return language
 
 
 def get_args():
@@ -6,10 +14,12 @@ def get_args():
     parser.add_argument(
         "-l",
         "--language",
-        type=str,
+        type=valid_language,
         default="english",
         help="Language"
     )
-    parser.add_argument("--count", type=int, default=25, help="Word count to be typed")
+    parser.add_argument("-c", "--count", type=int, default=25, help="Word count to be typed")
+    parser.add_argument("-L", "--list-languages", action="store_true",
+                        help="List available languages")
 
     return parser.parse_args()
--- src/ttyp/main.py
+++ src/ttyp/main.py
@@ -1,12 +1,16 @@
 from prompt_toolkit import print_formatted_text as print
 from .args import get_args
 from .ttyp import Ttyp
-from .content import random_words
+from .content import random_words, get_available_languages
 from .app import TtypApp
 
 
 def main():
     args = get_args()
+    if args.list_languages:
+        languages = get_available_languages()
+        print("\n".join(languages))
+        return
     to_type = random_words(language=args.language, word_count=args.count)
     ttyp = Ttyp(to_type=to_type)
     app = TtypApp(to_type=to_type, ttyp=ttyp)
