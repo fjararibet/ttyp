9214ee8c8d249994a5b3ce926e04b74a52590942
Felipe Jara <fjararibet@gmail.com>
2025-06-25 12:38:44 -0400

    add while to condition in ttyp wrap

--- src/ttyp/utils.py
+++ src/ttyp/utils.py
@@ -5,7 +5,7 @@ def ttyp_textwrap(text: str, width: int):
     result = textwrap.wrap(text, width=width)
     i = 0
     while i < len(result)-1:
-        if len(result[i]) >= width:
+        while len(result[i]) >= width:
             last_word = result[i].split()[-1]
             result[i] = " ".join(result[i].split()[:-1])
             result[i+1] = " ".join([last_word] + result[i+1].split())
